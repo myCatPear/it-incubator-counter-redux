fe439e1b956d9739b22709f4bbccd3d7423ff6a8
Denis Tavabilov <dagger1233@gmail.com>
2022-05-19 02:32:05 +0500

    add material ui

--- src/components/Counter/CounterPresent.tsx
+++ src/components/Counter/CounterPresent.tsx
@@ -1,4 +1,7 @@
+import { Button, TextField } from '@mui/material';
 import React from 'react';
+import s from './CounterPresent.module.css'
+
 
 type CounterPresentPropsType = {
     startValue: number,
@@ -34,28 +37,32 @@ const CounterPresent: React.FC<CounterPresentPropsType> = (props) => {
     } = props
     return (
         <>
-            <div>
-                startValue: <input
-                type={"number"}
-                value={startValue}
-                onChange={(e) => changeStartValue(+e.currentTarget.value)}
-            />
+            <div className={s.container}>
+                <div>
+                    startValue: <input
+                    type={"number"}
+                    value={startValue}
+                    onChange={(e) => changeStartValue(+e.currentTarget.value)}
+                />
+                </div>
+                <div>
+
+                    MaxValue: <input
+                    type={"number"}
+                    value={maxValue}
+                    onChange={(e) => changeMaxValue(+e.currentTarget.value)}
+                />
+                </div>
+                <div>
+                    <Button variant="contained" onClick={setCounterFromStartValueHandler} disabled={isDisabledButtonSet}>Set</Button>
+                    <Button variant="contained" onClick={incrementCounterValue} disabled={isDisabledButtonIncrementCount || maxCountValue}>+</Button>
+                    <Button variant="contained" onClick={resetCounterValue} disabled={isDisabledButtonReset}>reset</Button>
+                </div>
+                <div>
+                    {counter}
+                </div>
             </div>
-            <div>
-                MaxValue: <input
-                type={"number"}
-                value={maxValue}
-                onChange={(e) => changeMaxValue(+e.currentTarget.value)}
-            />
-            </div>
-            <div>
-                <button onClick={setCounterFromStartValueHandler} disabled={isDisabledButtonSet}>Set</button>
-                <button onClick={incrementCounterValue} disabled={isDisabledButtonIncrementCount || maxCountValue}>+</button>
-                <button onClick={resetCounterValue} disabled={isDisabledButtonReset}>reset</button>
-            </div>
-            <div>
-                {counter}
-            </div>
+
         </>
 
     );
